4fff5e99595605ee9eb7e65a4f4efece3010c49e
Sergey Meshchaninov <Le_t0s@mail.ru>
2017-12-20 15:30:58 +0400

    embed: add objects to engine
    add wrapper for delphi class, props and methods

--- src/delphi_intf.cpp
+++ src/delphi_intf.cpp
@@ -25,4 +25,45 @@ namespace embed {
   {
     Init();
   }
+  IObjectProp::IObjectProp(const char * pName, void * pObj, bool pRead, bool Pwrite)
+  {
+    name = pName;
+    obj = pObj;
+    read = pRead;
+    write = Pwrite;
+  }
+  IObjectMethod::IObjectMethod(const char * mName, void * mCall)
+  {
+    name = mName;
+    call = mCall;
+  }
+  IObjectTemplate::IObjectTemplate(char * objclasstype, void * delphiClass, v8::Isolate * isolate)
+  {
+    classTypeName = objclasstype;
+    dClass = delphiClass;
+    iso = isolate;
+  }
+  void IObjectTemplate::SetMethod(char * methodName, void * methodCall)
+  {
+    auto method = std::make_unique<IObjectMethod>(methodName, methodCall);
+    methods.push_back(std::move(method));
+  }
+  void IObjectTemplate::SetProperty(char * propName, void * propObj, bool read, bool write)
+  {
+    auto prop = std::make_unique<IObjectProp>(propName, propObj, read, write);
+    props.push_back(std::move(prop));
+  }
+  void IObjectTemplate::SetIndexedProperty(char * propName, void * propObj, bool read, bool write)
+  {
+    auto prop = std::make_unique<IObjectProp>(propName, propObj, read, write);
+    indexed_props.push_back(std::move(prop));
+  }
+  void IObjectTemplate::SetField(char * fieldName)
+  {
+    fields.push_back(fieldName);
+  }
+  void IObjectTemplate::SetParent(IObjectTemplate * parent)
+  {
+    parentTemplate = parent;
+  }
 }
--- src/delphi_intf.h
+++ src/delphi_intf.h
@@ -4,6 +4,59 @@
 //here will be export functions and classes to use NodeJS in delphi projects
 
 namespace embed {
+  struct IObjectProp{
+  public:
+    IObjectProp(const char * pName, void * pObj, bool pRead = true,
+      bool Pwrite = true);
+    std::string name = "";
+    bool read = true;
+    bool write = true;
+    void * obj;
+  };
+
+  struct IObjectMethod{
+  public:
+    IObjectMethod(const char * mName, void * mCall);
+    std::string name = "";
+    void * call = nullptr;
+  };
+
+  class IObjectTemplate : public IBaseIntf {
+  public:
+    IObjectTemplate(char * objclasstype, void * delphiClass, v8::Isolate * isolate);
+
+    virtual void APIENTRY SetMethod(char * methodName, void * methodCall);
+    ////maybe there isn't needed propObj
+    virtual void APIENTRY SetProperty(char* propName, void * propObj,
+      bool read, bool write);
+    virtual void APIENTRY SetIndexedProperty(char* propName,
+      void * propObj, bool read, bool write);
+    virtual void APIENTRY SetField(char* fieldName);
+    virtual void APIENTRY SetParent(IObjectTemplate * parent);
+
+    //pointer to delphi classtype
+    void * dClass = nullptr;
+    // class name
+    std::string classTypeName;
+
+    // it will be need for type check like:
+    // childObject instanceof parentClass
+    IObjectTemplate * parentTemplate = nullptr;
+  protected:
+    //??
+    std::vector<char> runStringResult;
+  private:
+    //result object template for JS
+    v8::Local<v8::FunctionTemplate> objTempl;
+
+    std::vector<std::unique_ptr<IObjectProp>> props;
+    std::vector<std::unique_ptr<IObjectProp>> indexed_props;
+    std::vector<std::string> fields;
+    std::vector<std::unique_ptr<IObjectMethod>> methods;
+
+    v8::Isolate * iso = nullptr;
+  };
+
   class IEmbedEngine : public BaseEngine {
   private:
     //this will be pointer to delphi engine object
