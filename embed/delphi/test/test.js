ced6d03e289b15f68a3bb15e410c04c85452cf92
Sergey Meshchaninov <Le_t0s@mail.ru>
2017-12-25 17:30:06 +0400

    fix test script

--- embed/delphi/test/test.js
+++ embed/delphi/test/test.js
@@ -1,6 +1,5 @@
 console.log('start test file');
 console.log(Func());
 console.log(Prop);
-Prop = 'property Prop was modified';
-console.log(Prop);
+console.log(Prop = 'property Prop was modified');
 console.log('success');
