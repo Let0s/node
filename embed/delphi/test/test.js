fa49e69d6b4f0dde9ea3d367571f579b323c5128
Le_t0s <Le_t0s@mail.ru>
2018-01-29 21:15:26 +0300

    fix test scripts

--- embed/delphi/test/test.js
+++ embed/delphi/test/test.js
@@ -3,35 +3,41 @@ var path = require('path');
 var testCount = 0;
 var passedCount = 0;
 
-function RunTest(testObj){
-  for (key in testObj) {
+function RunTest(testObj) {
+  for (var key in testObj) {
     if (typeof testObj[key] == 'function') {
       console.log('---------------------');
       console.log('run ' + key + ' test.');
       testCount++;
-      if (testObj[key]()) {
+      try {
+        testObj[key]()
         console.log('test ' + key + ' success.');
         passedCount++;
       }
+      catch (e) {
+        console.log(e)
+      }
       console.log('---------------------\n');
     }
-  }  
+  }
 }
 
 console.log('start test file');
 
 var files = fs.readdirSync('./');
 
-for (var i = 0; i < files.length; i++){
-  try{
-    var test = require(`./${files[i]}`);
-    RunTest(test);
+for (var i = 0; i < files.length; i++) {
+  try {
+    if (path.extname(files[i].toLowerCase() === '.js')) {
+      var test = require(`./${files[i]}`);
+      RunTest(test);
+    }
   }
-  catch (e){
+  catch (e) {
     console.log(e);
   }
 }
 
 console.log('End test file\n' +
-            `  summary test count: ${testCount}\n` + 
-            `  passed test count:  ${passedCount}\n`);
+  `  summary test count: ${testCount}\n` +
+  `  passed test count:  ${passedCount}\n`);
