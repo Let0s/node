6997fee0edcf77ecd4d84da9b570670b861c36e0
Sergey Meshchaninov <Le_t0s@mail.ru>
2018-04-25 10:21:12 +0400

    add simple test to check if object and its prop are available

--- embed/delphi/test/testClasses.js
+++ embed/delphi/test/testClasses.js
@@ -1,4 +1,13 @@
 ﻿var testClasses = {
+    testCircle: ()=>{
+        var circle = CreateCircle(5);
+        if (!circle){
+            throw new Error('circle is undefined');
+        }
+        if (Math.round(circle.Radius) != 5){
+            throw new Error('circle radius is not 5');
+        }
+    }
 }
 
 module.exports = testClasses;
