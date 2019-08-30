9328e3f1aab9961450acbff5e27522610e16eb9c
Sergey Meshchaninov <Le_t0s@mail.ru>
2019-08-30 15:08:38 +0300

    embed: Refactor external data storing (#80)
    Add classes for storing delphi data in V8 objects

--- src/delphi_intf.h
+++ src/delphi_intf.h
@@ -10,6 +10,46 @@ namespace embed {
   class IJSArray;
   class IJSFunction;
 
+  enum ExternalDataType {
+    Unknown = 0,
+    DelphiObject = 1,
+    DelphiIndexedPropObject = 2
+  };
+
+  class ExternalData {
+  public:
+    virtual ~ExternalData();
+  };
+
+  class DelphiObjectData: public ExternalData {
+  public:
+    DelphiObjectData(void * classType, void * obj);
+    void * GetDelphiClass();
+    void * GetDelphiObj();
+  private:
+    void * _dObj;
+    void * _dClass;
+  };
+
+  class DelphiIndexedPropObjectData : public DelphiObjectData {
+  public:
+    DelphiIndexedPropObjectData(void * classType, void * obj, void * indexedProp);
+    void * GetIndexedProp();
+  private:
+    void * _indexedProp;
+  };
+
+  class ExternalObjectData {
+  public:
+    ExternalObjectData(ExternalDataType type, ExternalData * data);
+    ~ExternalObjectData();
+    virtual ExternalDataType GetType();
+    virtual ExternalData * GetData();
+  private:
+    ExternalDataType _type;
+    ExternalData * _data;
+  };
+
 
   //wrapper for JS value
   class IJSValue: public IBaseIntf {
@@ -378,6 +418,7 @@ namespace embed {
     void * DelphiEngine();
     void* GetDelphiObject(v8::Local<v8::Object> holder);
     void* GetDelphiClasstype(v8::Local<v8::Object> obj);
+    ExternalObjectData * GetExternalData(v8::Local<v8::Object> obj);
     IClassTemplate * GetDelphiClassTemplate(void * classType);
     static IEmbedEngine * GetEngine(v8::Isolate * isolate);
     v8::Local<v8::Object> GetIndexedPropertyObject(void * obj,
@@ -398,6 +439,7 @@ namespace embed {
       v8::CopyablePersistentTraits<v8::Object>>> jsIndexedPropObjects;
     std::vector<std::unique_ptr<ObjectVariableLink>> objectLinks;
     std::vector<std::unique_ptr<IJSValue>> jsValues;
+    std::vector<std::unique_ptr<ExternalObjectData>> datalist;
     // code, that will be executed before script execution (without NodeJS features)
     std::string preCode;
   };
